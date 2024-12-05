d60082346e8857e95b02018e9226f7998606933b
MrDragon1225 <jefersonam49@gmail.com>
2024-12-05 14:58:02 -0500

    modified:   api/models/contacto.js

--- api/models/contacto.js
+++ api/models/contacto.js
@@ -2,31 +2,12 @@
 import { Schema, model } from 'mongoose';
 
 const contactSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    section: {
-        type: String,
-        required: true,
-        enum: ['reembolso', 'otros'],  // Limitamos las opciones a las disponibles
-    },
-    other_message: {
-        type: String,
-        required: false,  // Solo se requiere si la sección es "Otros"
-    },
-    message: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    }
+    name: {type: String,required: true,},
+    email: {type: String,required: true,},
+    section: {type: String,required: true,enum: ['reembolso', 'otros'],
+    },other_message: {type: String,required: false,},
+    message: {type: String,required: true,},
+    createdAt: {type: Date,default: Date.now,}
 });
 
 const Contact = model('Contact', contactSchema);
